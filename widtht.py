d228262b77d3fcc4894d3afc1a21821e4a2dfcac
fahimaamir <fahimaamir72@gmail.com>
2025-06-27 18:35:03 +0500

    Add files via upload

--- widtht.py
+++ widtht.py
@@ -1,9 +1,12 @@
-import os 
-import numpy as np
+
+
 import streamlit as st
-import pandas as pd
 import streamlit.components.v1 as components
 from pivottablejs import pivot_ui
+import pandas as pd
+import os 
+import numpy as np
+
 np.random.seed(42)
 rows = 50
 
@@ -37,18 +40,9 @@ st.markdown(css, unsafe_allow_html=True)
 #iris = pd.read_csv(    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
 #iris = pd.read_excel("c:/mfa/book11.xlsx")
 
+
+
 t = pivot_ui(df)
 
 with open(t.src) as t:
-    components.html(t.read(), width=1900, height=1000, scrolling=True)
-
-
-
-#body { font-family: Verdana;}
-#.c3-line, .c3-focused {stroke-width: 3px !important;}
-#.c3-bar {stroke: white !important; stroke-width: 1;}
-#.c3 text { font-size: 12px; color: grey;}
-#.tick line {stroke: white;}
-#.c3-axis path {stroke: grey;}
-#.c3-circle { opacity: 1 !important; }
-
+    components.html(t.read(), width=900, height=1000, scrolling=True)
