b27ce27de5e2d8062e2e0a62af2dcff85e9dc8ca
fahimaamir <fahimaamir72@gmail.com>
2025-06-27 18:18:45 +0500

    Add files via upload

--- widtht.py
+++ widtht.py
@@ -4,6 +4,22 @@ import streamlit as st
 import pandas as pd
 import streamlit.components.v1 as components
 from pivottablejs import pivot_ui
+np.random.seed(42)
+rows = 50
+
+
+df = pd.DataFrame({
+            'Product_ID': range(1, rows + 1),
+            'City': np.random.choice(['Karachi', 'Islamabad', 'Quata', 'Pishawar'], rows),
+            'Category': np.random.choice(['Electronics', 'Clothing', 'Home', 'Sports'], rows),
+            'Item': np.random.choice(['IRON', 'pant', 'Flate', 'football'], rows),
+            'Sale_person': np.random.choice(['Fahim', 'Aamir', 'Zahir', 'Asim'], rows),
+            'Base_Price': np.random.uniform(10, 500, rows).round(2),
+            'Quantity_Sold': np.random.randint(1, 100, rows),
+            'commission': np.random.randint(100, 1000, rows),
+        })
+
+
 
 
 st.set_page_config(layout="wide")
@@ -18,14 +34,10 @@ css = '''
 st.markdown(css, unsafe_allow_html=True)
 
 
-
-
-
-
 #iris = pd.read_csv(    "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv")
-iris = pd.read_excel("c:/mfa/book11.xlsx")
+#iris = pd.read_excel("c:/mfa/book11.xlsx")
 
-t = pivot_ui(iris)
+t = pivot_ui(df)
 
 with open(t.src) as t:
     components.html(t.read(), width=1900, height=1000, scrolling=True)
